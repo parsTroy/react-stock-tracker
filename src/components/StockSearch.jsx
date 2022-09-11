616bdb641ff3e891b114b51c42de61b55ec47821
parsTroy <troyparsons@hotmail.com>
2022-09-11 14:08:08 -0400

    update

--- src/components/StockSearch.jsx
+++ src/components/StockSearch.jsx
@@ -4,11 +4,11 @@ import StockItem from "./StockItem";
 const StockSearch = ({stocks}) => {
     const [searchText, setSearchText] = useState('')
 
-    console.log(stocks);
+    // console.log(stocks);
 
     return (
         <div className='rounded-div my-4'>
-            <div className='flex flex-col md:flex-row justify-between pt-4 pb-6text-center md:text-right'>
+            <div className='flex flex-col md:flex-row justify-between pt-4 pb-6text-center md:text-right mb-8 ml-4 mr-4'>
                 <h1 className='text-2xl font-bold my-2'>Search Stock</h1>
                 <form>
                     <input onChange={(e) => setSearchText(e.target.value)} className='w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl' type='text' placeholder='Search a Stock' />
