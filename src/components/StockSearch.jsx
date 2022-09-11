12a2283f10f662a913115a8af02d2433bff8ae74
parsTroy <troyparsons@hotmail.com>
2022-09-11 14:24:32 -0400

    fixed

--- src/components/StockSearch.jsx
+++ src/components/StockSearch.jsx
@@ -11,7 +11,7 @@ const StockSearch = ({stocks}) => {
             <div className='flex flex-col md:flex-row justify-between pt-4 pb-6text-center md:text-right mb-8 ml-4 mr-4'>
                 <h1 className='text-2xl font-bold my-2'>Search Stock</h1>
                 <form>
-                    <input onChange={(e) => setSearchText(e.target.value)} className='w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl' type='text' placeholder='Search a Stock' />
+                    <input  onChange={(e) => setSearchText(e.target.value.toUpperCase())} className='w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl' type='text' style={{textTransform: 'uppercase'}} placeholder='ticker search' />
                 </form>
             </div>
 
@@ -35,7 +35,7 @@ const StockSearch = ({stocks}) => {
                         if (searchText === '') {
                             return value
                         } else if (
-                            results.ticker.includes(searchText)
+                            value.ticker.includes(searchText)
                         ) {
                             return value
                         }
